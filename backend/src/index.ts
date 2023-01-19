f44199b5adc3368a4db3ff43008b23d667c74a8e
marwa <marwajawad19@gmail.com>
2023-01-19 21:06:54 +0300

    add endpoint routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,11 @@
-import * as express from 'express'
-import * as morgan from 'morgan'
+import express from 'express'
+import morgan from 'morgan'
+// Routes
+import UserRoute from './Routes/User'
+import TestResultRoute from './Routes/TestResult'
+import MedicationRoute from './Routes/Medication'
+import * as swaggerDocument from '../src/swagger.json'
+import * as swaggerUI from 'swagger-ui-express'
 
 const app = express()
 const PORT = 4000
@@ -10,4 +16,9 @@ app.get('/', (_, res) => {
   res.send({ res: 'Health Hero backend is running! 🌈' })
 })
 
+app.use('/user', UserRoute)
+app.use('/testResult', TestResultRoute)
+app.use('/medication', MedicationRoute)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
