ae79f9c9e5d9e190f49f424b7939abbb89e48eac
marwa <marwajawad19@gmail.com>
2023-01-23 15:37:03 +0300

    Move table components to separate component

--- frontend/src/components/table/table.tsx
+++ frontend/src/components/table/table.tsx
@@ -1,62 +1,7 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import './index.css'
-import { GoEye } from 'react-icons/go'
-import { FiDownloadCloud } from 'react-icons/fi'
-import moment from 'moment'
-
-function TabhleHead(props: { data: string[] }) {
-  return (
-    <thead className="bg-white border-b">
-      <tr>
-        {props.data.map((name) => (
-          <th
-            scope="col"
-            className="px-6 py-3 text-md font-bold text-left text-black-500 uppercase bg-white"
-          >
-            {name}
-          </th>
-        ))}
-      </tr>
-    </thead>
-  )
-}
-
-type TdProps = {
-  lable?: string
-  icon?: ReactNode
-}
-function Tablebody(props: {
-  data: { name: string; description: string; date: string }[]
-}) {
-  const Td = (props: TdProps) => (
-    <td className="px-6 py-4 text-sm font-medium text-secondaryColor_slate whitespace-nowrap border border-none">
-      {props.lable}
-      {props.icon}
-    </td>
-  )
-
-  return (
-    <>
-      {props.data.map((item, i) => (
-        <tr className={i % 2 === 0 ? 'bg-secondaryColor_lace' : 'bg-white'}>
-          <Td lable={item.name} />
-          <Td lable={item.description} />
-          <Td lable={moment(item.date).format('YYYY/MM/DD')} />
-          <Td
-            icon={
-              <>
-                <span className="flex">
-                  <GoEye className="mr-3 text-linearColor_orange text-xl" />
-                  <FiDownloadCloud className="text-xl text-sencodaryColor_slate" />
-                </span>
-              </>
-            }
-          />
-        </tr>
-      ))}
-    </>
-  )
-}
+import Thead from './thead'
+import Tbody from './tbody'
 export default function Table(props: {
   thead: string[]
   tbody: {
@@ -68,9 +13,9 @@ export default function Table(props: {
   return (
     <div className="overflow-x-scroll  border border-white rounded-lg ">
       <table className="table-fixed w-full">
-        <TabhleHead data={props.thead} />
+        <Thead data={props.thead} />
         <tbody className="divide-y divide-gray-200">
-          <Tablebody data={props.tbody} />
+          <Tbody data={props.tbody} />
         </tbody>
       </table>
     </div>
