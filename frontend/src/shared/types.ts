0ea288ab009341172ff3231b50bef67f569048b5
sandramsc <sandraashipala@gmail.com>
2023-01-25 20:04:05 +0200

    created 5 star rating with heroicons BTH section_1

--- frontend/src/shared/types.ts
+++ frontend/src/shared/types.ts
@@ -1,6 +1,10 @@
 export interface TestimonialType {
   icon: JSX.Element
   icon_2: JSX.Element
+  icon_3: JSX.Element
+  icon_4: JSX.Element
+  icon_5: JSX.Element
+  icon_6: JSX.Element
   comment: string
   name: string
   location: string
