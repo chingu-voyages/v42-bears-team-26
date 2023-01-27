1e7e6d695d2053e4bf86295668980f301147be44
sandramsc <sandraashipala@gmail.com>
2023-01-27 05:18:20 +0200

    removed social-media-icons folder

--- frontend/src/Pages/landing-page/components/BottomHalfH/Section_4.tsx
+++ frontend/src/Pages/landing-page/components/BottomHalfH/Section_4.tsx
@@ -3,29 +3,26 @@
 function Section_4() {
   return (
     <div className="bg-gray">
-      <section
-        id="footernote"
-        className="ml-20 mr-20 items-center md:flex"
-      >
+      <section id="footernote" className="ml-20 mr-20 items-center md:flex">
         {/* Footer Note */}
         <div className="mt-2 mb-2 gap-20 md:flex">
           <div className="text-left relative ">
             <p>All Rights Reserved Pixelieze Design</p>
           </div>
 
-            <div className="text-right ml-80 gap-10 md:flex">
-                <div>
-                <p>Terms and Conditions</p>
-                </div>
-
-                <div>
-                <p>Privacy and Cookies</p>
-                </div>
-
-                <div>
-                <p>Accessability</p>
-                </div>
+          <div className="text-right ml-80 gap-10 md:flex">
+            <div>
+              <p>Terms and Conditions</p>
             </div>
+
+            <div>
+              <p>Privacy and Cookies</p>
+            </div>
+
+            <div>
+              <p>Accessability</p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
