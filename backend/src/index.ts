0c1028dec0f826377d844dd0d62dffe44ef2f877
fmtabbara <fmtabbara@hotmail.co.uk>
2023-02-02 22:37:08 +0000

    fix cors?

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 const corsOptions = {
-  origin: 'https://health-hero-backend.onrender.com/*',
+  origin: 'https://health-hero-backend.onrender.com',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
