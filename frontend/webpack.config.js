eb701e9ed1a65feb435b09375b24ce089b1ab1a2
sandramsc <sandraashipala@gmail.com>
2023-02-03 19:22:09 +0200

    added svg files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -53,7 +53,7 @@ const config = {
         use: [stylesHandler, 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
       {
