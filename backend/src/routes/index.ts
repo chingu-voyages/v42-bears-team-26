6e0426e54e4138b6d1ce815599dd0cc2619b6eab
marwa <marwajawad19@gmail.com>
2023-02-08 01:58:40 +0300

    adding auth route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as MedicationRoute } from './Medication'
 export { default as TestResultRoute } from './TestResult'
 export { default as UserRoute } from './User'
+export { default as Auth } from '../auth'
