8b02aacbd84d7c806ff5431afa7145bfa8341ccf
Suwanna Truss <suwanna.bgu@gmail.com>
2023-02-08 18:46:32 +0000

    add ReminderRoute and GET endpoint

--- backend/src/routes/Reminder.ts
+++ backend/src/routes/Reminder.ts
@@ -3,15 +3,17 @@ import { dbClient } from '../db'
 
 const ReminderRoute = express.Router()
 
-// const reminderList = [
-//     {
-//       medicineName: 'Medicine A',
-//       time: '2:30',
-//       taken: false,
-//     },
-
 ReminderRoute.get('/', async (_, res) => {
-  const { rows: results } = await dbClient.query('select * from med_reminder;')
+  const { rows: results } = await dbClient.query(
+    `select medicines.med_id,  med_reminder.reminder_id, medicines.med_name,
+    med_reminder.reminder_time, med_reminder.is_current, 
+	jsonb_path_query(med_taken_data, '$.tracker_data[*] ? (@.date == "2023-02-08")') as today_tracker
+	FROM med_reminder
+	inner join medicines
+    on med_reminder.med_id = medicines.med_id
+    WHERE med_reminder.is_current = true
+	ORDER BY med_reminder.reminder_time
+    `)
 
   res.send({
     res: results,
